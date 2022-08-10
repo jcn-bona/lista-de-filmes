909ddbeb48db1b1b0afe24bdc7801a05c37d4a4c
Jeronimo Cardoso Neto <jeronimo.cardoso.neto@gmail.com>
2022-08-10 09:07:26 -0300

    Commit inicial

---
+++ app/src/main/java/com/jcn/listadefilmes/view/MoviesViewHolder.kt
@@ -0,0 +1,6 @@
+package com.jcn.listadefilmes.view
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MoviesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
